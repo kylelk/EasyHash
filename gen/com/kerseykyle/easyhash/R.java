c8ccff2be43712a3ed4555df8fa0289699c334a2
kyle kersey <kylelk@comcast.net>
2013-12-23 14:57:58 -0800

    Added dividers

--- gen/com/kerseykyle/easyhash/R.java
+++ gen/com/kerseykyle/easyhash/R.java
@@ -72,5 +72,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int Divider=0x7f060002;
     }
 }
